861f39253b696872aa226c8de34c4830310712df
handaoui <handaoui>
2018-03-29 09:55:09 +0200

    added drawer activity and movies in projection fragment

--- app/src/main/java/com/handaoui/movies/MainActivity.kt
+++ app/src/main/java/com/handaoui/movies/MainActivity.kt
@@ -13,14 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var recyclerView:RecyclerView = moviesPreview as RecyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
-        var moviesList = ArrayList<MoviePreview>()
-        moviesList.add(MoviePreview("Harry Potter", R.drawable.harry))
-        moviesList.add(MoviePreview("The Shawshank", R.drawable.shawshank))
-        moviesList.add(MoviePreview("The beast", R.drawable.beast))
 
-        var moviesPreviewAdapter = MoviesPreviewAdapter(applicationContext, moviesList)
-        recyclerView.adapter = moviesPreviewAdapter
     }
 }
